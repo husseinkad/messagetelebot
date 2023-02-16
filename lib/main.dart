b75613b584181ac1be6379e28851f52040f37a12
xbanker3 <72931678+xbanker3@users.noreply.github.com>
2023-02-16 05:13:06 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:telebot/sendMessage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  TextEditingController messageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Telegram bot app'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: messageController,
+              decoration: const InputDecoration(
+                hintText: 'Enter Your Message.',
+                contentPadding:
+                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                  BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                  BorderSide(color: Colors.black, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                         RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                        )
+                    )
+                ),
+                onPressed: () {
+                  if(messageController.text != null){
+                    sendMessageToTelegram(messageController.text);
+                  }
+                },
+                child: Text(
+                    "Send message".toUpperCase(),
+                    style: const TextStyle(fontSize: 14)
+                )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/sendMessage.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+
+
+
+void sendMessageToTelegram(message) async {
+  try {
+    String token = '6046692881:AAFLopvjZYgisKDQIUgDTVshppC6Wbt5Qwg';
+    String userId = '803359657';
+    String url = 'https://api.telegram.org/bot$token/sendMessage?chat_id=$userId&text=$message';
+    final response = await Dio().post(url);
+    print(response);
+  }catch(e){
+    print(e.toString());
+  }
+}
